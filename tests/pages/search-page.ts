d2bcbae734b1c45304282b44243b9310ec46298b
Jason <jasonetienne1@gmail.com>
2024-05-08 16:08:02 +0100

    updating search to account for cookies model, test passing, more to build out

--- tests/pages/search-page.ts
+++ tests/pages/search-page.ts
@@ -3,16 +3,21 @@ exports.SearchPage = class SearchPage {
   productsLink: any;
   searchField: any;
   searchButton: any;
+  cookieModel: any;
 
   constructor(page) {
     this.page = page;
     this.productsLink = page.locator("a[href='/products']");
     this.searchField = page.locator("input#search_product");
     this.searchButton = page.locator("i.fa-search");
+    this.cookieModel = page.locator("//p[contains(text(),'Consent')]");
   }
 
   async searchForProduct(product) {
     await this.page.waitForTimeout(2000);
+    if (await this.cookieModel.isVisible(2000)){
+      await this.cookieModel.click();
+    }
     await this.productsLink.click();
     await this.searchField.fill(product);
     await this.searchButton.click();
