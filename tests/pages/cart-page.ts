cf1d7711b71f2355f3687cab3d84d8627692162a
Jason <jasonetienne1@gmail.com>
2024-05-08 16:43:55 +0100

    setting up cart and fixing issues with locators

--- tests/pages/cart-page.ts
+++ tests/pages/cart-page.ts
@@ -1,16 +1,17 @@
 exports.CartPage = class CartPage {
-    page: any;
-    cartButton: any;    
-  
-    constructor(page) {
-      this.page = page;
-      this.cartButton = page.locator("a[href='/products']");
-      
-    }
-  
-    async selectCart() {
-        await this.cartButton.click();
-        await this.page.waitForURL("**/view_cart")
-      
-    }
-  };
+  page: any;
+  cartButton: any;
+
+  constructor(page) {
+    this.page = page;
+    this.cartButton = page.locator(
+      "//a[contains(text(),'Cart')]/i[contains(@class, 'fa-shopping-cart')]"
+    );
+  }
+
+  async selectCart() {
+    await this.cartButton.click();
+    await this.page.waitForTimeout(2000);
+    await this.page.waitForURL("**/view_cart");
+  }
+};
