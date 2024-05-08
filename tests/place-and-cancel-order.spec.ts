9c7cf94c8ca00367f5b1e3aed7b307f2e1114032
Jason <jasonetienne1@gmail.com>
2024-05-08 16:04:10 +0100

    setting up test

---
+++ tests/place-and-cancel-order.spec.ts
@@ -0,0 +1,19 @@
+const { expect, test } = require("@playwright/test");
+const { LoginPage } = require("../tests/pages/login-page");
+const { ProductPage } = require("../tests/pages/product-page");
+const { SearchPage } = require("../tests/pages/search-page");
+
+const { email, password } = require("./credentials");
+
+
+test("test", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const searchPage = new SearchPage(page);
+  const productPage = new ProductPage(page);
+
+  await page.goto("https://www.automationexercise.com/");
+  await page.screenshot({ path: "screenshot.jpg" });
+  await loginPage.SignIn(email, password);
+  await searchPage.searchForProduct("tshirt");
+  await productPage.addToCart();
+});
