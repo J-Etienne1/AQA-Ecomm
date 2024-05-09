a68cd519b995dc494625408d55cd9b81c66a4842
Jason <jasonetienne1@gmail.com>
2024-05-09 19:37:24 +0100

    finishing off cart page

--- tests/pages/cart-page.ts
+++ tests/pages/cart-page.ts
@@ -1,17 +1,20 @@
 exports.CartPage = class CartPage {
   page: any;
   cartButton: any;
+  checkoutButton: any;
 
   constructor(page) {
     this.page = page;
     this.cartButton = page.locator(
       "//a[contains(text(),'Cart')]/i[contains(@class, 'fa-shopping-cart')]"
     );
+    this.checkoutButton = page.locator("a[href='/view_cart']");
   }
 
   async selectCart() {
     await this.cartButton.click();
     await this.page.waitForTimeout(2000);
     await this.page.waitForURL("**/view_cart");
+    await this.checkoutButton.click();
   }
 };
